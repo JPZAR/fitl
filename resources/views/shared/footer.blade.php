1d3a3c4474b7fc4cf66c73bf59ee4a8d46439719
JP Roux <jprouxglobal@gmail.com>
2018-05-24 16:42:10 +0200

    Added css files

---
+++ resources/views/shared/footer.blade.php
@@ -0,0 +1,37 @@
+<!-- Footer -->
+<footer class="page-footer font-small blue">
+
+    <!-- Footer Links -->
+    <div class="container-fluid text-center text-md-left">
+
+        <!-- Grid row -->
+        <div class="row">
+
+            <!-- Grid column -->
+            <div class="col-md-6">
+
+                <!-- Content -->
+                <h5 class="text-uppercase">&copy; {{date('Y')}} Prog_Quest</h5>
+
+            </div>
+            <!-- Grid column -->
+
+            <hr class="clearfix w-100 d-md-none pb-3">
+
+            <!-- Grid column -->
+            <div class="col-md-6">
+
+                <!-- Links -->
+                <h5 class="text-uppercase">Making FITL Pretty</h5>
+
+            </div>
+            <!-- Grid column -->
+
+        </div>
+        <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+</footer>
+<!-- Footer -->
